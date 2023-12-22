10a2df12003f650931da166dbf2c042867809121
Replit user <>
2023-12-22 05:29:13 +0000

    Added a middleware logger

--- myApp.js
+++ myApp.js
@@ -8,7 +8,18 @@ console.log("Hello World");
 app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
-app.get("/json", (req, res) => res.json({ message: "Hello json" }));
+app.get("/json", (req, res) => {
+  if (process.env["MESSAGE_STYLE"] === "uppercase") {
+    return res.json({ message: "HELLO JSON" });
+  } else {
+    return res.json({ message: "Hello json" });
+  }
+});
 module.exports = app;
